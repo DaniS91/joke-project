1ef1074c4342bfc1a5c65836fe3b3fab392f0157
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-08 10:14:08 -0800

    update webpack files for project

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Setup Template',
+      title: 'Joke of the Day',
       template: './src/index.html',
       inject: 'body'
     })

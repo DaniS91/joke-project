d7bf9323d5610f8f6a5a9f15eb878ea560fd54d9
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-08 11:03:24 -0800

    add project js and html
     Co-authored by: Tanya Prado
    <tanyaprado@gmail.com>
    Coauthored by: Geoffrey Gao
    <geoffreygao1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
 //User Interface Logic
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import JokeService from './joke.js';
+
+// Business Logic
+
+function getJoke() {
+  let promise = JokeService.getJoke();
+  promise.then(function(joke) {
+    printElements(joke);   
+  }, function(errorArray) {
+    printError(errorArray);
+  });
+}
+
+// UI Logic
+
+function printElements(data) {
+  document.querySelector('#jokeResponse').innerHTML = '';
+  const jokeType = data['type'];
+
+  if (jokeType === "single"){
+    document.querySelector('#jokeResponse').innerHTML = data['joke'];
+  }else if (jokeType === 'twopart') {
+    let question = document.createElement("div");
+    question.setAttribute("id","jokeQuestion");
+    question.innerHTML = `Q: ${data['setup']}`;
+    let answer = document.createElement("div");
+    answer.setAttribute("id","jokeAnswer");
+    answer.innerHTML = `A: ${data['delivery']}`;
+    document.querySelector('#jokeResponse').append(question);
+    document.querySelector('#jokeResponse').append(answer);
+  }
+}
+
+function printError(error) {
+  document.querySelector('#jokeResponse').innerText = `There was an error accessing the joke data for ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  getJoke();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
--- src/joke.js
+++ src/joke.js
@@ -1,2 +1,21 @@
 //Business Logic
 
+export default class JokeService {  
+  static getJoke() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`;
+      request.addEventListener("loadend", function() {
+        const response = JSON.parse(this.responseText);
+        if (this.status === 200) {
+          resolve(response);
+        } else {
+          reject([this, response]);
+        }
+      });
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+

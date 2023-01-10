00f2b1b94d49f4cf019c7f858848f584f31cd74f
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-01-09 20:33:07 -0800

    finish adding logic to use three buttons in joke game
    Co-authored by: Tanya Prado
    <tanyaprado@gmail.com>
    Co-authored-by: Geoffrey Gao
    <geoffreygao1@gmail.com>
    Co-authored-by: Emil Anton
    <emilbstn@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 //User Interface Logic
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { library } from 'webpack';
 import './css/styles.css';
 import JokeService from './joke.js';
+
+let globalJoke = {};
+let globalJokeArray = [];
 
 // Business Logic
 
 function getJoke() {
   let promise = JokeService.getJoke();
   promise.then(function(joke) {
-    printElements(joke);   
+    printElements(joke);  
+    globalJoke = joke;
+    let practiceButton = document.getElementById("practiceButton");
+    practiceButton.addEventListener("click", practiceJoke);
   }, function(errorArray) {
     printError(errorArray);
   });
@@ -20,7 +27,6 @@ function getJoke() {
 function printElements(data) {
   document.querySelector('#jokeResponse').innerHTML = '';
   const jokeType = data['type'];
-
   if (jokeType === "single"){
     document.querySelector('#jokeResponse').innerHTML = data['joke'];
   }else if (jokeType === 'twopart') {
@@ -35,6 +41,73 @@ function printElements(data) {
   }
 }
 
+function practiceJoke() {
+  let data = globalJoke;
+  const jokeType = data['type'];
+  const ul = document.querySelector("ul");
+  ul.innerHTML = null;
+  
+  if (jokeType === 'twopart') {
+    let jokeArray = data['setup'].split(' ');
+    globalJokeArray = jokeArray;
+    jokeArray.forEach((element, index) => {
+      const li = document.createElement("li");
+      li.append(element + ' ');
+      ul.append(li);
+      li.setAttribute("id", `${index}`);
+    });
+    let num = Math.floor(Math.random() * jokeArray.length);
+    for (let i = 0; i<jokeArray.length; i++) {
+      if (i === num) {
+        let itemToDelete = document.getElementById(`${i}`);
+        itemToDelete.innerText = '';
+        let textBox = document.createElement("input");
+        textBox.setAttribute("id", `${i}`);
+        textBox.setAttribute("type", "text");
+        itemToDelete.append(textBox);
+      }
+    }
+    let compareButton = document.getElementById("compareButton");
+    compareButton.addEventListener("click", compareInput);
+
+  } else if (jokeType === 'single') {
+    let jokeArray = data['joke'].split(' ');
+    globalJokeArray = jokeArray;
+    jokeArray.forEach((element, index) => {
+      const li = document.createElement("li");
+      li.append(element+ ' ');
+      ul.append(li);
+      li.setAttribute("id", `${index}`);
+    });
+    let num = Math.floor(Math.random() * jokeArray.length);
+      
+    for (let i = 0; i<jokeArray.length; i++) {
+      if (i === num) {
+        let itemToDelete = document.getElementById(`${i}`);
+        itemToDelete.innerText = '';
+        let textBox = document.createElement("input");
+        textBox.setAttribute("id", `${i}`);
+        textBox.setAttribute("type", "text");
+        itemToDelete.append(textBox);
+      }
+    }
+    let compareButton = document.getElementById("compareButton");
+    compareButton.addEventListener("click", compareInput);
+  }
+}
+
+function compareInput() {
+  let jokeArray = globalJokeArray;
+  let input = document.querySelector('input').value;
+  let inputNum = document.querySelector('input').getAttribute('id');
+  let results = document.getElementById('results');
+  if (input === jokeArray[parseInt(inputNum)]) {
+    results.innerHTML = "Correct!";
+  } else {
+    results.innerHTML = `Nope! The correct word was ${jokeArray[parseInt(inputNum)]}`;
+  }
+}
+
 function printError(error) {
   document.querySelector('#jokeResponse').innerText = `There was an error accessing the joke data for ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
@@ -45,5 +118,6 @@ function handleFormSubmission(event) {
 }
 
 window.addEventListener("load", function() {
-  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  document.querySelector("form#makeJokeForm").addEventListener("submit", handleFormSubmission);
 });
+
